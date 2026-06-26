4a5fa9fd7c08c4f5c984abf80556bf92c78b05a2
LucasAnacleto <lucas-anacleto@outlook.com>
2026-06-26 17:24:22 -0300

    add layout admin

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -1,16 +1,20 @@
 import { AdminNavbar } from "./admin-navbar";
 
 interface AuthLayoutProps {
-  children: React.ReactNode;
+  children: React.ReactNode
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <main className="min-h-screen bg-[#f4f4f0] p-1 text-black">
-      <AdminNavbar />
-      {children}
+    <main className="min-h-screen bg-[#f4f4f0] p-1 text-black mx-auto max-w-screen-2xl p-4">
+      <div className="flex justify-between items-center">
+        <AdminNavbar />
+      </div>
+      <div className="mx-auto max-w-screen-2xl p-4 bg-[#f4f4f0]">
+        {children}
+      </div>
     </main>
-  );
-};
+  )
+}
 
 export default AuthLayout;
